9101b73381f03a69c6d12334bfa150e550e0d791
artgro <art.gromov@bk.ru>
2021-05-29 12:11:58 +0300

    bin_search done

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,28 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
+    high = len(arr) - 1
+    low = 0
+    while low <= high:
+        mid = (high + low) // 2
+        find_elem = arr[mid]
+        if elem > find_elem:
+            low = mid + 1
+        elif elem < find_elem:
+            high = mid - 1
+        else:
+            while mid > 0 and arr[mid - 1] == elem:
+                mid -= 1
+            return mid
+
     return None
+
+
+def main():
+    # elem = 7
+    # arr = [i for i in range(10)]
+    print(binary_search(2, [2, 2, 2, 2, 2, 2, 2, 2]))
+
+
+if __name__ == '__main__':
+    main()
