7bb4070cc5065c5387b6881c4c53f77c745ba807
AG <art.gromov@bk.ru>
2021-05-31 11:53:19 +0300

    priority_queue in process

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -4,9 +4,8 @@ Priority Queue
 Queue priorities are from 0 to 10
 """
 from typing import Any
-from collections import defaultdict
 
-_queue = {i: [] for i in range(11)}
+_priority_queue = {i: [] for i in range(11)}
 
 
 def enqueue(elem: Any, priority: int = 0) -> None:
@@ -16,16 +15,9 @@ def enqueue(elem: Any, priority: int = 0) -> None:
     :param elem: element to be added
     :return: Nothing
     """
-
-
-
-if __name__ == '__main__':
-    enqueue("a", 4)
-    print(_priority_queue)
-    enqueue("a", 5)
-    print(_priority_queue)
-    enqueue("a", 1)
-    print(_priority_queue)
+    _priority_queue.setdefault(priority, elem)
+    _priority_queue[priority].append(elem)
+    return None
 
 
 def dequeue() -> Any:
@@ -54,3 +46,15 @@ def clear() -> None:
     :return: None
     """
     _priority_queue.clear()
+
+
+if __name__ == '__main__':
+    print(_priority_queue)
+    enqueue("a", 4)
+    print(_priority_queue)
+    enqueue("a", 5)
+    print(_priority_queue)
+    enqueue("b", 4)
+    print(_priority_queue)
+    # enqueue("a", 1)
+    # print(_priority_queue)
