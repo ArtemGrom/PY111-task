97ed2347e07cd184b02abf0bb3c1cfe58c9dda44
AG <art.gromov@bk.ru>
2021-06-02 14:06:38 +0300

    Taylor done

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,6 +2,7 @@
 Taylor series
 """
 from typing import Union
+import math
 
 
 def ex(x: Union[int, float]) -> float:
@@ -11,8 +12,10 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    e = 0
+    for i in range(0, 100):
+        e = e + (x ** i) / math.factorial(i)
+    return e
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +25,19 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    sin_x = 0
+    for i in range(0, 10):
+        const = (-1) ** i
+        sin_x = sin_x + ((x ** (2 * i + 1))/math.factorial(2 * i + 1)) * const
+    return sin_x
+
+
+def main():
+    print(ex(10))
+    print(sinx(10))
+
+
+if __name__ == '__main__':
+    main()
+
+
