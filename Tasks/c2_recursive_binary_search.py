23ef77fb3d8791fdd5c0e57a9185dc3d417948ba
AG <art.gromov@bk.ru>
2021-06-07 12:24:10 +0300

    recursive_bin_search done v1.1

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -1,7 +1,7 @@
 from typing import Sequence, Optional
 
 
-def binary_search(elem: int, arr: Sequence) -> Optional[int]:
+def binary_search(elem: int, arr: Sequence, low=0, high=None) -> Optional[int]:
     """
     Performs binary search of given element inside of array (using recursive way)
 
@@ -9,5 +9,28 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    if high is None:
+        high = len(arr) - 1
+    if low > high:
+        return False
+    mid = (high + low) // 2
+    if arr[mid] == elem:
+        while mid > 0 and arr[mid - 1] == elem:
+            mid -= 1
+        return mid
+    elif elem < arr[mid]:
+        return binary_search(elem, arr, low, mid - 1)
+    else:
+        return binary_search(elem, arr, mid + 1, high)
+
+
+def main():
+    elem = 98
+    arr = [4, 8, 17, 45, 69, 98, 122, 185]
+    print(binary_search(elem, arr, 0, len(arr)-1))
+
+
+if __name__ == '__main__':
+    main()
+
+
