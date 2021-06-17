3c085228cdad6894219ef915e71113a16340b653
AG <art.gromov@bk.ru>
2021-06-17 09:09:57 +0300

    priority_queue done v1.2

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -3,6 +3,10 @@ Priority Queue
 
 Queue priorities are from 0 to 10
 """
+from typing import Any
+
+_priority_queue = {i: [] for i in range(11)}
+
 
 def enqueue(elem: Any, priority: int = 0) -> None:
     """
@@ -11,6 +15,9 @@ def enqueue(elem: Any, priority: int = 0) -> None:
     :param elem: element to be added
     :return: Nothing
     """
+    if priority in _priority_queue:
+        _priority_queue[priority].append(elem)
+
 
 def dequeue() -> Any:
     """
@@ -18,7 +25,9 @@ def dequeue() -> Any:
 
     :return: dequeued element
     """
-    return None
+    for priority in _priority_queue:
+        if len(_priority_queue[priority]) > 0:
+            return _priority_queue[priority].pop(0)
 
 
 def peek(ind: int = 0, priority: int = 0) -> Any:
@@ -28,7 +37,8 @@ def peek(ind: int = 0, priority: int = 0) -> Any:
     :param ind: index of element (count from the beginning)
     :return: peeked element
     """
-    return None
+    if len(_priority_queue[priority]) > ind:
+        return _priority_queue[priority][ind]
 
 
 def clear() -> None:
@@ -37,3 +47,17 @@ def clear() -> None:
 
     :return: None
     """
+    for priority in _priority_queue:
+        _priority_queue[priority].clear()
+
+
+if __name__ == '__main__':
+    print(_priority_queue)
+    enqueue("a", 4)
+    print(_priority_queue)
+    enqueue("a", 5)
+    print(_priority_queue)
+    enqueue("b", 4)
+    print(_priority_queue)
+    enqueue("a", 1)
+    print(_priority_queue)
