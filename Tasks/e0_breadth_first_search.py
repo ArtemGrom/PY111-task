cd651d2420b7a3ced1f5cfb83fbeb7d3fdc16803
AG <art.gromov@bk.ru>
2021-06-23 11:36:12 +0300

    breadth_first_search v1.1 реализация на хэш-таблицах

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,8 +1,23 @@
+from collections import deque
 from typing import Hashable, List
+
 import networkx as nx
 
+start_node = 'A'
+g = {}
+g[start_node] = ['B', 'F']
+g['B'] = ['G']
+g['F'] = ['G']
+g['G'] = ['C', 'H', 'I']
+g['C'] = ['H']
+g['I'] = ['H']
+g['H'] = ['D', 'E', 'J']
+g['E'] = ['D']
+g['D'] = []
+g['J'] = []
 
-def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
+
+def bfs(g: [dict, nx.Graph], start_node: Hashable) -> List[Hashable]:
     """
     Do an breadth-first search and returns list of nodes in the visited order
 
@@ -10,5 +25,20 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    search_queue = deque()
+    search_queue += g[start_node]
+    searched = [start_node]
+    while search_queue:
+        char = search_queue.popleft()
+        if not char in searched:
+            search_queue += g[char]
+            searched.append(char)
+    return searched
+
+
+def main():
+    print(bfs(g, 'A'))
+
+
+if __name__ == '__main__':
+    main()
