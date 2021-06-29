09b06d1311ec603944d27ff187367b2d908ca995
AG <art.gromov@bk.ru>
2021-06-29 11:43:00 +0300

    bubble sort v1.0

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,4 +8,17 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    for i in range(len(container) - 1):
+        for j in range(len(container) - i - 1):
+            if container[j] > container[j + 1]:
+                container[j], container[j + 1] = container[j + 1], container[j]
+
     return container
+
+
+def main():
+    print(sort([63, 80, 62, 69, 71, 37, 12, 90, 19, 67]))
+
+
+if __name__ == '__main__':
+    main()
